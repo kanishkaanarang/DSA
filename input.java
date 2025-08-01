4e867f5adde5f176cb295e6383613e97bbb505ca
kanishkaanarang <kanishkaanarang@gmail.com>
2025-08-02 03:17:07 +0530

    4th commit

---
+++ input.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        // if same program me do type ke input lene hai toh use parse
+        String name = sc.nextLine();
+        System.out.println("hello " + name);
+
+        for (int i = 0; i<=n; i++ ){
+            System.out.println(i);
+        }
+
+        
+    }
+}
