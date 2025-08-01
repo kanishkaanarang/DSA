706b809190f2c51b0bfd091a26439492304df692
kanishkaanarang <kanishkaanarang@gmail.com>
2025-08-01 23:02:50 +0530

    3rd commit

---
+++ conditionals.java
@@ -0,0 +1,24 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int x = 15;
+
+        if(x%2 ==0){
+            System.out.println("x is even");
+        }else{ 
+            System.out.println("x is odd");
+        }
+
+        System.out.println("yeah");
+
+        int a= 10;
+        int b = 20;
+
+        if(a==b){
+            System.out.println("a is equal to b");
+        }else if(a>b){
+            System.out.println("a is greater than b");
+        }else{
+            System.out.println("a is less than b");
+        }
+    }
+}
