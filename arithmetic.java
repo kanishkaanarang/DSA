0908d164d1bad6f38f74e030163e43a82a2c0d4c
kanishkaanarang <kanishkaanarang@gmail.com>
2025-08-01 22:18:30 +0530

    second commit

---
+++ arithmetic.java
@@ -0,0 +1,17 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int pdt = a*b;
+        int sub = a-b;
+        int div = a/b;
+        int mod = a%b;
+    
+        System.out.println("Sum of a and b is: " + sum);
+        System.out.println("product of a &b is: " + pdt);
+        System.out.println("subtraction of a &b is: " + sub);
+        System.out.println("division of a &b is: " + div);
+        System.out.println("modulus of a &b is: " + mod);
+    }
+}
