3ca3894747882721acb29d2453b8ea132bb3d094
kanishkaanarang <kanishkaanarang@gmail.com>
2025-08-02 16:38:53 +0530

    Optimize prime number checking logic by changing loop condition to reduce iterations

--- prime.java
+++ prime.java
@@ -12,7 +12,7 @@ public class prime {
                     System.out.println("1 is not a prime number");
                 } else {
                     boolean isPrime = true;
-                    for (int i=2; i<= n/2; i++){
+                    for (int i=2; i*i<= n; i++){
                         if (n % i == 0) {
                             System.out.println(n + " is not a prime number");
                             isPrime = false;
