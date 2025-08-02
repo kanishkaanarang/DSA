b3bd69b73c1b5541daa16d0872598773c065557d
kanishkaanarang <kanishkaanarang@gmail.com>
2025-08-02 16:22:26 +0530

    Refactor input handling in main method and add prime number checking logic

--- input.java
+++ input.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        // if same program me do type ke input lene hai toh use parse
-        String name = sc.nextLine();
-        System.out.println("hello " + name);
-
-        for (int i = 0; i<=n; i++ ){
-            System.out.println(i);
+        try 
+            (Scanner sc = new Scanner(System.in)){
+            int n = Integer.parseInt(sc.nextLine());
+            // if same program me do type ke input lene hai toh use parse
+            String name = sc.nextLine();
+            System.out.println("hello " + name);
+    
+            for (int i = 0; i<=n; i++ ){
+                System.out.println(i);
+            }
+            sc.close();
         }
-
         
     }
 }
---
+++ prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        try(Scanner scn = new Scanner(System.in)){
+            System.out.println("Enter how many numbers to be entered");
+            int t = scn.nextInt();
+
+            while(t>0){
+                int n = scn.nextInt();
+                if(n==1){
+                    System.out.println("1 is not a prime number");
+                } else {
+                    boolean isPrime = true;
+                    for (int i=2; i<= n/2; i++){
+                        if (n % i == 0) {
+                            System.out.println(n + " is not a prime number");
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if(isPrime) {
+                        System.out.println(n + " is a prime number");
+                    }
+                }
+                t=t-1;
+            }
+        }
+    }
+}
